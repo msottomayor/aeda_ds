0d621e958fbb00c0dba59136d18fd0402e3d449a
Maria Sottomayor <30004864@students.ual.pt>
2020-06-01 17:39:56 +0100

    arrays done, no errors

--- aed_ds/stacks/list_stack.py
+++ aed_ds/stacks/list_stack.py
@@ -3,9 +3,10 @@ from ..lists.singly_linked_list import SinglyLinkedList
 from ..exceptions import EmptyStackException, FullStackException
 
 class ListStack(Stack):
-    def __init__(self,limit=None):
-        self.stack = SinglyLinkedList
+    def __init__(self, limit=None):
+        self.stack = SinglyLinkedList()
         self.limit = limit
+        self.num_elements = 0
 
     def is_empty(self): 
         return self.stack.is_empty()
@@ -13,7 +14,7 @@ class ListStack(Stack):
     def is_full(self):
         if not self.limit:
             return False
-        return self.limit == self.stack.size()
+        return self.limit == self.num_elements()
 
     def size(self): 
         return self.stack.size()
@@ -31,5 +32,4 @@ class ListStack(Stack):
     def pop(self):
         if self.is_empty():
             raise EmptyStackException()
-        return self.stack.remove_first()
-
+        return self.stack.remove_last()
